28dc5b4a90aa3f009ac5e237bbe2178b111d9628
Mateus <omateus.conceicao@gmail.com>
2023-01-18 15:20:38 -0300

    adicionando novas funcionalidades

---
+++ byteBank/Contas/ContaCorrente.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using byteBank.Titular;
+
+namespace byteBank.Contas
+{
+    public class ContaCorrente
+    {
+        public static int TotalContasCriadas { get; private set; }
+
+        private int numeroAgencia;
+            
+        public int NumeroAgencia
+        {
+            get { return numeroAgencia; }
+            private set 
+            {
+                if (value < 0) return;
+
+                this.numeroAgencia = value; 
+            }
+        }
+
+        public string Conta { get; set; }
+        public double Saldo { get; set; }
+        public Cliente Titular { get; set; }
+
+        public void Depositar(double valor)
+        {
+            this.Saldo += valor;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (valor <= this.Saldo)
+            {
+                this.Saldo -= valor;
+                return true;
+            }
+            return false;
+        }
+
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if (this.Saldo < valor)
+            {
+                return false;
+            }
+
+            Sacar(valor);
+            destino.Depositar(valor);
+            return true;
+        }
+
+        public void SetSaldo(double valor)
+        {
+            if (valor < 0) return;
+
+            this.Saldo = valor;
+        }
+
+        public double GetSaldo()
+        {
+            return this.Saldo;
+        }
+
+        public ContaCorrente(int numero_agencia, string numero_conta)
+        {
+            this.NumeroAgencia = numero_agencia;
+            this.Conta = numero_conta;
+            TotalContasCriadas++;
+        }
+    }
+}
--- byteBank/Program.cs
+++ byteBank/Program.cs
@@ -1,29 +1,58 @@
-﻿using byteBank;
+﻿using byteBank.Contas;
+using byteBank.Titular;
 
-ContaCorrente contaAndre = new ContaCorrente();    
-contaAndre.titular = "Andre Silva";
-contaAndre.numeroAgencia = 15;
-contaAndre.conta = "1010-x";
-contaAndre.saldo = 100;
+//ContaCorrente contaAndre = new ContaCorrente();
+//contaAndre.titular = ;
+//contaAndre.numeroAgencia = 15;
+//contaAndre.conta = "1010-x";
+//contaAndre.saldo = 100;
 
-contaAndre.Depositar(250);
 
-Console.WriteLine($"Saldo da conta do andre {contaAndre.saldo}");
+//contaAndre.Depositar(250);
 
-if (contaAndre.Sacar(20)) Console.WriteLine($"Saldo da conta do andre {contaAndre.saldo}");
+//Console.WriteLine($"Saldo da conta do andre {contaAndre.saldo}");
 
-ContaCorrente contaMaria = new ContaCorrente();
-contaMaria.titular = "Maria Souza";
-contaMaria.numeroAgencia = 17;
-contaMaria.conta = "1005-x";
-contaMaria.saldo = 350;
+//if (contaAndre.Sacar(20)) Console.WriteLine($"Saldo da conta do andre {contaAndre.saldo}");
 
-Console.WriteLine($"Saldo da conta da maria {contaMaria.saldo}");
+//ContaCorrente contaMaria = new ContaCorrente();
+//contaMaria.titular = "Maria Souza";
+//contaMaria.numeroAgencia = 17;
+//contaMaria.conta = "1005-x";
+//contaMaria.saldo = 350;
 
-contaAndre.Transferir(50, contaMaria);
+//Console.WriteLine($"Saldo da conta da maria {contaMaria.saldo}");
 
-Console.WriteLine($"Saldo da conta da maria {contaMaria.saldo}");
+//contaAndre.Transferir(50, contaMaria);
 
-Console.WriteLine($"Saldo da conta do andre {contaAndre.saldo}");
+//Console.WriteLine($"Saldo da conta da maria {contaMaria.saldo}");
 
+//Console.WriteLine($"Saldo da conta do andre {contaAndre.saldo}");
+
+//Cliente cliente = new Cliente();
+
+//cliente.Nome = "Mateus Conceição";
+//cliente.Cpf = "123456789-00";
+//cliente.Profissao = "Dev";
+
+//ContaCorrente conta = new ContaCorrente();
+//conta.Titular = cliente;
+//conta.Conta = "1010-x";
+//conta.NumeroAgencia = 15;
+//conta.SetSaldo(100);
+
+//Console.WriteLine($"Titular: {conta.Titular.Nome}");
+//Console.WriteLine($"Saldo: {conta.GetSaldo()}");
+
+//ContaCorrente conta4 = new ContaCorrente(18, "1010-X");
+//conta4.SetSaldo(500);
+//conta4.Titular = new Cliente();
+
+//Console.WriteLine(conta4.GetSaldo());
+//Console.WriteLine(conta4.NumeroAgencia);
+
+ContaCorrente conta5 = new ContaCorrente(18, "1010-X");
+Console.WriteLine(ContaCorrente.TotalContasCriadas);
+
+ContaCorrente conta6 = new ContaCorrente(20, "1010-X");
+Console.WriteLine(ContaCorrente.TotalContasCriadas);
 
