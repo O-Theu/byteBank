a1cedc22bcf35dc156d84084a3e2995d3a7bbfc3
Mateus Conceicao <omateus.conceicao@gmail.com>
2023-01-24 17:57:57 -0300

    novo projeto

---
+++ ByteBankIO/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var enderecoDoArquivo = "contas.txt";
+        var numeroDeBytesLidos = -1;
+
+        var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
+
+        var buffer = new byte[1024]; //1KB
+
+        while (numeroDeBytesLidos != 0)
+        {
+            numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+            EscreverBuffer(buffer);
+
+        }
+    }
+
+    static void EscreverBuffer(byte[] buffer)
+    {
+        foreach (byte b in buffer)
+        {
+            Console.Write(b);
+            Console.Write(" ");
+        }
+    }
+}
