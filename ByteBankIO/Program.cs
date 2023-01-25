2a483cf978fc76381df3ed67818e95d43e42db38
Mateus Conceicao <omateus.conceicao@gmail.com>
2023-01-25 17:57:23 -0300

    add commit

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -1,29 +1,45 @@
 ﻿using System;
+using System.Text;
+
 class Program
 {
     static void Main(string[] args)
     {
         var enderecoDoArquivo = "contas.txt";
-        var numeroDeBytesLidos = -1;
 
-        var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
 
-        var buffer = new byte[1024]; //1KB
-
-        while (numeroDeBytesLidos != 0)
+        using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
         {
-            numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
-            EscreverBuffer(buffer);
+            var numeroDeBytesLidos = -1;
+
+
+            var buffer = new byte[1024]; //1KB
+
+            while (numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer);
+            }
+
+            fluxoDoArquivo.Close();
 
         }
+
     }
 
     static void EscreverBuffer(byte[] buffer)
     {
-        foreach (byte b in buffer)
-        {
-            Console.Write(b);
-            Console.Write(" ");
-        }
+        var utf8 = new UTF8Encoding();
+
+        #region
+        //foreach (byte b in buffer)
+        //{
+        //    Console.Write(b);
+        //    Console.Write(" ");
+        //}
+        #endregion
+
+        var texto = utf8.GetString(buffer);
+        Console.Write(texto);
     }
 }
